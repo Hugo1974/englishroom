1bde1f2ec288efffc63f99787ca914ba1a9063ca
Hugo Morago Martín <h.morago.martin@gmail.com>
2020-04-09 19:17:02 +0200

    Adding xam to english_words

---
+++ xam/er_main.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw(say switch :5.10);
+use Carp qw(croak carp);
+use Web::Query qw();
+use Gtk3 -init;
+use Glib qw(TRUE FALSE);    #use Gtk3::WebKit2;
+use utf8;
+use Encode;
+use open 'locale';
+use lib '../';
+use EnglishRoom::MainWindowGUI qw(
+  MainWindow
+  RevSearchEntry
+  StackSearch
+  find_menu_page1
+  find_menu_page2
+  EntrySearch
+  BtnSearch
+  BtnMenu
+  RevMain
+  StackMainMenu
+  MainMenuPage1
+  MainMenuPage2
+  RoomsPage1
+  RoomsPage2
+  RoomBox
+);
+
+##
+MainWindow->set_title('English Room 0.01');
+MainWindow->signal_connect( destroy => sub { Gtk3->main_quit(); return; } );
+
+# Show menú
+BtnMenu->signal_connect(
+    "clicked",
+    sub {
+        if ( ( StackMainMenu->get_visible_child ) eq MainMenuPage2() ) {
+            StackMainMenu->set_visible_child( MainMenuPage1() );
+        }
+        else {
+            StackMainMenu->set_visible_child( MainMenuPage2() );
+        }
+        return;
+    }
+);
+
+MainWindow->show_all;
+Gtk3::main;
+
+__END__
